0a4e380f02136ed014b8ac60953ff7ddcaf87f8e
DESKTOP-R92G1M0\son <freyja738@gmail.com>
2023-02-15 16:52:04 +0900

    first

---
+++ src/main/java/com/tsoft/batch/MyBatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tsoft.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatchProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsoft/batch/MyBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsoft.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
